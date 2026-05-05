a97c0a4cbdc1592e9be48bef9bb27f9ac61a2617
Ivan Menkov <hereimenkov@gmail.com>
2026-05-05 23:36:32 +0300

    сохраняем в  localStorage браузера добавленный новый текст

--- src/main.ts
+++ src/main.ts
@@ -28,14 +28,25 @@ navLinks.forEach((link) => {
 const noteDisplay = document.getElementById("main-note-text");
 const noteInput = document.getElementById("note-input") as HTMLTextAreaElement | null;
 const addBtn = document.getElementById("add-btn");
+const NOTE_STORAGE_KEY = "main_note_text";
 
 if (noteDisplay && noteInput && addBtn) {
+
+  const savedNote = localStorage.getItem(NOTE_STORAGE_KEY);
+  if (savedNote) {
+    noteDisplay.textContent = savedNote;
+  }
+
   addBtn.addEventListener("click", () => {
     const piece = noteInput.value.trim();
+
     if (!piece) {
       return;
     }
+
     noteDisplay.textContent += (noteDisplay.textContent?.length ? " " : "") + piece;
+    localStorage.setItem(NOTE_STORAGE_KEY, noteDisplay.textContent || "");
+    
     noteInput.value = "";
   });
 }
